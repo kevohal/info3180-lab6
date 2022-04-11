182bd22b9f64ec80f018322f6713ba654ad528f6
kevohal <kevar.halstead@mymona.edu.jm>
2022-04-11 17:03:59 -0500

    After adding Authorization header

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -9,9 +9,24 @@
 <script>
 
 export default {
-data() {
-return {};
+    data() {
+        return {};
 
-    },
-};
+        },
+        created() {
+            fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=fb043ad247004c06b50523a5db684dd0' ,
+    {
+        headers: {
+            Authorization: `Bearer ${import.meta.env.VITE_NEWSAPI_TOKEN}`
+        }
+    })
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+                console.log(data);
+            });
+        }
+//     Authorization: fb043ad247004c06b50523a5db684dd0
+}
 </script>
