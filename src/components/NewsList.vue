2324e08537c032a1a7f6b59fd12d4a0f121e3aff
kevohal <kevar.halstead@mymona.edu.jm>
2022-04-12 20:23:33 -0500

    Styled news titles and descriptions without photos.

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -3,7 +3,9 @@
         <li v-for="article in articles" class="news__item">
             <!-- <img :src="image" class="news__item"/> -->
             <!-- {{ image }} -->
-            <h3>{{ article.title }}</h3>
+            <!-- <img src="https://cdn.cnn.com/cnnnext/dam/assets/220411204126-01-kevin-stitt-file-super-tease.jpg" class= "image" />
+            {{ image }} -->
+            <h5>{{ article.title }}</h5>
             <p>{{ article.description}}</p>
 
         </li>
@@ -11,33 +13,67 @@
 </template>
 
 <script>
-
 export default {
     data() {
         return {
             articles: [],
             // image: {image: url(article.urlToImage)}
         };
-            
-        
+    }, 
+    created() {
+         let self = this;
 
-        },
-        created() {
-            let self = this;
-
-            fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=fb043ad247004c06b50523a5db684dd0' ,
+        fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=fb043ad247004c06b50523a5db684dd0' ,
     {
-        headers: {
+     
+      headers: {
             Authorization: `Bearer ${import.meta.env.VITE_NEWSAPI_TOKEN}`
         }
     })
             .then(function(response) {
-                return response.json();
+            return response.json();
             })
             .then(function(data) {
-                console.log(data);
-                self.articles = data.articles;
+            console.log(data);
+            self.articles = data.articles;
             });
-        }
+
+    }
 }
-</script>
+
+ </script>
+
+<style>
+    ul { 
+    /*    display: flex; 
+        flex-direction: column; 
+        flex-wrap: nowrap; 
+        justify-content: flex-start; 
+        margin: 1em 0px; 
+        padding: 0; 
+        list-style: none; */  
+    
+    display: grid;
+    grid-template-columns: repeat(auto-fill,minmax(250px, 1fr));
+    row-gap: 10px;
+    column-gap: 10px;
+    list-style-type: none;
+    
+        
+} 
+
+.news__item {
+    border: solid black 1px; 
+    margin: 15px;
+    border-bottom-left-radius: 5%;
+    border-bottom-right-radius: 5%;
+    border-bottom-color: green;
+
+}
+
+/* .image {
+    height: 100px;
+    width: 250px
+} */
+    
+</style>
