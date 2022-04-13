6a5fffe0162396020cddf183bf1829d0bd4b5f2a
kevohal <kevar.halstead@mymona.edu.jm>
2022-04-13 10:55:36 -0500

    Two-way Data Binding and Event Handling.

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -1,15 +1,28 @@
 <template>
-    <ul class="news__list">
-        <li v-for="article in articles" class="news__item">
-            <!-- <img :src="image" class="news__item"/> -->
-            <!-- {{ image }} -->
-            <!-- <img src="https://cdn.cnn.com/cnnnext/dam/assets/220411204126-01-kevin-stitt-file-super-tease.jpg" class= "image" />
-            {{ image }} -->
-            <h5>{{ article.title }}</h5>
-            <p>{{ article.description}}</p>
 
+    <form @submit.prevent="searchNews" class="d-flex flex-column justify-content-center">
+
+        <div class="input-group mx-sm-3 mb-2">
+            <label class="visually-hidden" for="search">Search</label>
+            <input type="search" name="search" v-model="searchTerm" id="search"
+            class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
+            <button class="btn btn-primary mb-2">Search</button>
+        </div>
+        
+        <p>You are searching for {{ searchTerm }}</p>
+
+    </form>
+
+    <ul class="news__list">
+        <li v-for="article in articles" :key="article.url" class="news__item">
+            <img :src="article.urlToImage" class="news__item"/> 
+            <h5>{{ article.title }}</h5>
+            <p>{{ article.description}}</p> 
         </li>
     </ul>
+
+    
+
 </template>
 
 <script>
@@ -17,16 +30,30 @@ export default {
     data() {
         return {
             articles: [],
-            // image: {image: url(article.urlToImage)}
+            searchTerm: ''
         };
     }, 
+
+    methods: {
+        searchNews() {
+            let self = this;
+        
+            fetch('https://newsapi.org/v2/everything?q='+ self.searchTerm + '&language=en', 
+        
+            {
+                headers: {
+                    Authorization: `Bearer ${import.meta.env.VITE_NEWSAPI_TOKEN}`,
+                }
+            })
+        }
+    },
     created() {
-         let self = this;
+        let self = this;
 
         fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=fb043ad247004c06b50523a5db684dd0' ,
     {
      
-      headers: {
+        headers: {
             Authorization: `Bearer ${import.meta.env.VITE_NEWSAPI_TOKEN}`
         }
     })
@@ -41,39 +68,27 @@ export default {
     }
 }
 
- </script>
+</script>
 
 <style>
-    ul { 
-    /*    display: flex; 
-        flex-direction: column; 
-        flex-wrap: nowrap; 
-        justify-content: flex-start; 
-        margin: 1em 0px; 
-        padding: 0; 
-        list-style: none; */  
-    
-    display: grid;
-    grid-template-columns: repeat(auto-fill,minmax(250px, 1fr));
-    row-gap: 10px;
-    column-gap: 10px;
-    list-style-type: none;
-    
-        
-} 
+    ul {   
+        display: grid;
+        grid-template-columns: repeat(auto-fill,minmax(250px, 1fr));
+        row-gap: 10px;
+        column-gap: 10px;
+        list-style-type: none;
+    } 
 
-.news__item {
-    border: solid black 1px; 
-    margin: 15px;
-    border-bottom-left-radius: 5%;
-    border-bottom-right-radius: 5%;
-    border-bottom-color: green;
+    .news__item {
+        border: solid black 1px; 
+        margin: 15px;
+        border-bottom-left-radius: 5%;
+        border-bottom-right-radius: 5%;
+    }
 
-}
+    img.news__item {
+        height: 200px;
+        width: 200px;
+    }
 
-/* .image {
-    height: 100px;
-    width: 250px
-} */
-    
 </style>
